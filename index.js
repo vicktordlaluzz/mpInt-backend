468e775fe3b4e1864fb75cb109cb2b260d4a10bc
vickdlaluzz <vicktordlaluzz@gmail.com>
2020-09-29 19:39:54 -0500

    Inicio del proyecto

---
+++ index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+// Crear el servidor de express
+const app = express();
+
+// Configuracion CORS
+app.use(cors());
+
+// Lectura y parseo del body
+app.use(express.json());
+
+// Base de datos
+dbConnection();
+
+// Rutas
+// app.use('/api/usuarios/roles', require('./routes/roles'));
+// app.use('/api/usuarios', require('./routes/usuarios'));
+// app.use('/api/clientes', require('./routes/clientes'));
+// app.use('/api/tramites', require('./routes/tramites'));
+// app.use('/api/documentos', require('./routes/documentos'));
+// app.use('/api/tickets', require('./routes/tickets'));
+// app.use('/api/auth', require('./routes/auth'));
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Servidor corriendo en puerto ' + process.env.PORT);
+})

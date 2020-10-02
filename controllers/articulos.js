44818906ecbe8e04e84a410966414c0c877a08a4
vickdlaluzz <vicktordlaluzz@gmail.com>
2020-10-01 23:49:19 -0500

    Correcciones realizadas

--- controllers/articulos.js
+++ controllers/articulos.js
@@ -3,6 +3,8 @@ const Articulo = require('../models/articulo');
 const path = require('path');
 const fs = require('fs');
 
+const mercadopago = require('mercadopago');
+
 const getArticulos = async(req, res = response) => {
     try {
         const articulos = await Articulo.find();
@@ -23,10 +25,70 @@ const getArticulo = async(req, res = response) => {
     try {
         const artId = req.params.id;
         const articulo = await Articulo.findById(artId);
-        res.json({
-            ok: true,
-            articulo
-        })
+        //Crea un objeto de preferencia
+        const preference = {
+
+            "items": [{
+                "id": "1234",
+                "title": articulo.nombre,
+                "currency_id": "MXN",
+                "picture_url": "https://integracionmp-nodejs.herokuapp.com/api/articulos/imgs/" + articulo.img,
+                "description": "Dispositivo móvil de Tienda e-commerce",
+                "category_id": "art",
+                "quantity": 1,
+                "unit_price": articulo.precio
+            }],
+            "payer": {
+                "name": "Lalo",
+                "surname": "Landa",
+                "email": "test_user_58295862@testuser.com",
+                "phone": {
+                    "area_code": "52",
+                    "number": 5549737300
+                },
+                "address": {
+                    "street_name": "Insurgentes Sur",
+                    "street_number": 1602,
+                    "zip_code": "03940"
+                }
+            },
+            "back_urls": {
+                "success": "https://www.success.com",
+                "failure": "http://www.failure.com",
+                "pending": "http://www.pending.com"
+            },
+            "auto_return": "approved",
+            "payment_methods": {
+                "excluded_payment_methods": [{
+                    "id": "amex"
+                }],
+                "excluded_payment_types": [{
+                    "id": "atm"
+                }],
+                "installments": 6
+            },
+            "notification_url": "https://www.your-site.com/ipn",
+            "external_reference": "vicktordlaluzz@gmail.com"
+        };
+
+
+        mercadopago.preferences.create(preference)
+            .then(function(response) {
+                // Este valor reemplazará el string "<%= global.id %>" en tu HTML
+                global.id = response.body.id;
+                res.json({
+                    ok: true,
+                    response,
+                    articulo
+                })
+            }).catch(function(error) {
+                console.log(error);
+                res.status(500).json({
+                    ok: false,
+                    msg: 'Error inesperado administrador'
+                })
+            });
+
     } catch (error) {
         console.log(error);
         res.json({
